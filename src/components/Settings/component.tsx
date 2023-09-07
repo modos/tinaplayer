91fe5beaa79a6c6d36cbc4be77a98897561f17cc
modos <modos.m98@gmail.com>
2023-09-07 18:45:45 +0330

    func: return to main page button added to settings page

--- src/components/Settings/component.tsx
+++ src/components/Settings/component.tsx
@@ -1,4 +1,5 @@
 import {Button} from "@material-tailwind/react";
+import {useNavigate} from "react-router-dom";
 
 type Props = {
     theme: string;
@@ -6,10 +7,15 @@ type Props = {
 };
 export function Settings({theme, onChangeTheme}: Props) {
     const themes = ['light', 'dark', 'cyberpunk', 'aqua', 'coffee'];
+    const navigate = useNavigate();
 
     return(
         <>
-            <h2 className="text-base-content">Themes</h2>
+            <Button variant="text" onClick={() => navigate(-1)}>
+                <i className="fa-solid fa-arrow-left mr-4"></i>
+                Return
+            </Button>
+            <h2 className="text-base-content mt-4">Themes</h2>
             <div className="flex gap-4 mt-4">
                 {themes.map(item => <Button key={item} onClick={() => onChangeTheme(item)} variant="outlined" color="gray">
                     {theme === item && <i className="fa-solid fa-circle-check mr-4"></i>}
