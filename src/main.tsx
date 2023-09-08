b5e83d37a7240db923ce3d3fe466bd7702aa737e
modos <modos.m98@gmail.com>
2023-09-08 20:26:17 +0330

    func: basic manifest and worker files created

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,16 @@ import './index.css';
 import { ThemeProvider } from "@material-tailwind/react";
 import {BrowserRouter} from "react-router-dom";
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
       <BrowserRouter>
