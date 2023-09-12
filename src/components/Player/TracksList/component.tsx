60f3a4ffa9fbf3f449fef864b301d57ed7bd7e39
modos <modos.m98@gmail.com>
2023-09-12 23:22:47 +0330

    fix: infinite reRender in trackslist

--- src/components/Player/TracksList/component.tsx
+++ src/components/Player/TracksList/component.tsx
@@ -2,14 +2,17 @@ import {Card, List, ListItem, ListItemPrefix} from "@material-tailwind/react";
 import {usePlayer} from "@/store/player.ts";
 import {storeFile} from "@/types/types.ts";
 import {getAllTracks} from "@/helpers/indexedDB.ts";
-import {useState} from "react";
+import {useLayoutEffect, useState} from "react";
+import {useTracks} from "@/store/tracks.ts";
 
 export function TracksList() {
     const [tracks, setTracks] = useState([] as Array<storeFile>);
     const setCurrentPlayingTrack = usePlayer(state => state.setCurrentTrack);
     const setPrevPlayingTrack = usePlayer(state => state.setPrevTrack);
     const setNextPlayingTrack = usePlayer(state => state.setNextTrack);
-    
+
+    useTracks.subscribe(state => setTracks(state.tracks));
+
     function trackSelected(track: storeFile, i: number) {
         setCurrentPlayingTrack(track);
         tracks[i - 1] && setPrevPlayingTrack(tracks[i - 1]);
@@ -17,7 +20,9 @@ export function TracksList() {
 
     }
 
-    getAllTracks().then(data => setTracks(data));
+    useLayoutEffect(() => {
+        getAllTracks().then(data => setTracks(data));
+    }, []);
 
     return (
         <Card className="bg-base-100 w-full h-full sm:p-5">
@@ -26,7 +31,7 @@ export function TracksList() {
                    <div key={track.file.name} onClick={() => trackSelected(track, i)}>
                        <ListItem>
                            <ListItemPrefix>
-                               <img src={track.cover} alt={track.file.name} width="32px" height="32px"/>
+                               <img src={URL.createObjectURL(track.cover)} alt={track.file.name} width="32px" height="32px"/>
                            </ListItemPrefix>
                            <p>{track.file.name}</p>
                        </ListItem>
