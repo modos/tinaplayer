7e7881e7ff0994f9207bf414d0cd9f708ae65394
modos <modos.m98@gmail.com>
2023-09-18 14:40:28 +0330

    feat: swipe right to hide drawer

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import {TracksList} from "@/components/Player/TracksList";
 import {Route, Routes} from "react-router-dom";
 import {Settings} from "@/components/Settings";
 import {useRef, useState} from "react";
-import useSwipeLeft from "@/hooks/useSwipeLeft.ts";
-import useScreenSize from "@/hooks/useScreenSize.ts";
+import {useScreenSize, useSwipeLeft, useSwipeRight} from "@/hooks";
 function App() {
     const selectedTheme = localStorage.getItem('theme') || 'light';
     document.documentElement.classList.value = selectedTheme;
@@ -18,12 +17,15 @@ function App() {
         setShowDrawer(true);
     };
 
-    const handleSwipeLeftEnd = (): void => {
-        setTimeout(() => setShowDrawer(false), 5000);
+
+    const handleSwipeRight = (): void => {
+        setShowDrawer(false);
     };
 
 
-    useSwipeLeft(swappableDivRef, handleSwipeLeft, handleSwipeLeftEnd);
+
+    useSwipeLeft(swappableDivRef, handleSwipeLeft);
+    useSwipeRight(swappableDivRef, handleSwipeRight);
 
     function changeTheme(name: string) {
         document.documentElement.classList.value = name;
@@ -33,21 +35,21 @@ function App() {
 
   return (
     <>
-     <div className="flex">
-         <div className="bg-base-100 w-full flex flex-col bg-base-100 h-screen">
+     <div className="flex relative">
+         <div className="bg-base-100 w-full flex flex-col bg-base-100 h-[calc(100dvh)]">
              <div className="w-full sm:px-5 sm:py-2 overflow-hidden basis-4/5" ref={swappableDivRef}>
                     <Routes>
                         <Route path="/" element={<TracksList/>}/>
                         <Route path="/settings" element={<Settings theme={theme} onChangeTheme={(newTheme: string) => changeTheme(newTheme)}/>}/>
                     </Routes>
              </div>
-             <div className="basis-1/5">
+             <div className="basis-1/5 z-[2]">
                  <Player/>
              </div>
          </div>
          {
              (isDesktop || showDrawer) &&
-             <div className="bg-base-100 bg-base-100 border-l-2 border-neutral">
+             <div className="bg-base-100 bg-base-100 border-l-2 border-neutral absolute right-0">
                  <Drawer/>
              </div>
          }
---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+export * from './useScreenSize.ts';
+export * from './useSwipeLeft.ts';
+export * from './useSwipeRight.ts';
