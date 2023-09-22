ca974ea72ca56ac7833f5a410c19dc3ed8e79723
modos <modos.m98@gmail.com>
2023-09-22 22:24:26 +0330

    style: cover size updated

--- src/components/Player/TracksList/TrackCover/component.tsx
+++ src/components/Player/TracksList/TrackCover/component.tsx
@@ -6,10 +6,12 @@ interface props {
 export const TrackCover = memo(({ cover }: props) => {
     const currentCover = cover ? URL.createObjectURL(cover) : '/mp3.png';
     return (
-        <img
-            className="w-[32px] h-[32px] sm:w-[48px] sm:h-[48px] rounded-lg object-cover object-center shadow-md shadow-blue-gray-900/50"
-            src={currentCover}
-            alt="nature image"
-        />
+        <div className="w-[48px] h-[48px]">
+            <img
+                className="rounded-lg object-cover object-center shadow-md shadow-blue-gray-900/50"
+                src={currentCover}
+                alt="nature image"
+            />
+        </div>
     );
 });
