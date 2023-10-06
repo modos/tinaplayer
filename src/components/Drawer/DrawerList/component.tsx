d4d1e99c21705eda83ed25da11c94c9dea2830d7
modos <modos.m98@gmail.com>
2023-10-07 00:05:00 +0330

    refactor: click on music icon return user to home

--- src/components/Drawer/DrawerList/component.tsx
+++ src/components/Drawer/DrawerList/component.tsx
@@ -1,31 +1,21 @@
 import { IconButton } from '@material-tailwind/react';
-import { getTracks } from '@/helpers/fileSystem.ts';
-import { mapImportedTracks } from '@/helpers/track.ts';
-import { useTracks } from '@/store/tracks.ts';
 import { Link } from 'react-router-dom';
 
 export function DrawerList() {
-    const storeTracks = useTracks((state) => state.add);
-    async function insertTracks() {
-        const tracks = await getTracks();
-
-        if (tracks.length) {
-            storeTracks(await mapImportedTracks(tracks));
-        }
-    }
-
     return (
         <div>
             <div className="flex flex-col gap-3">
-                <IconButton color="blue" onClick={insertTracks}>
-                    <i className="fas fa-music" />
-                </IconButton>
-                <Link to={'settings'}>
+                <Link to="/">
+                    <IconButton color="blue">
+                        <i className="fas fa-music" />
+                    </IconButton>
+                </Link>
+                <Link to="settings">
                     <IconButton color="purple">
                         <i className="fas fa-cog" />
                     </IconButton>
                 </Link>
-                <Link to={'favourites'}>
+                <Link to="favourites">
                     <IconButton color="red">
                         <i className="fas fa-heart" />
                     </IconButton>
